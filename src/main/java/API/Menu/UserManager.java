c32e9ebceed87cc55949f308044bcdbba26003e6
borgarlie <borgar.lie@gmail.com>
2018-11-11 12:51:48 +0100

    Add input checking when changing username

--- src/main/java/API/Menu/UserManager.java
+++ src/main/java/API/Menu/UserManager.java
@@ -13,18 +13,21 @@ public class UserManager {
 
     public static Handler updateUsername = ctx -> {
         String body = ctx.body();
-
         JSONParser parser = new JSONParser();
         Object obj = parser.parse(body);
         JSONObject jsonObject = (JSONObject) obj;
 
         String username = (String) jsonObject.get("username");
+        if (username == null || username.trim().length() < 5) {
+            ctx.status(400);
+            ctx.json(false);
+            return;
+        }
 
         DecodedJWT jwt = JavalinJWT.getDecodedFromContext(ctx);
         String userId = jwt.getClaim(USER_ID_CLAIM).asString();
 
         boolean userUpdated = UserDatabase.updateUsername(userId, username);
-
         ctx.json(userUpdated);
     };
 }
